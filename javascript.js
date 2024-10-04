de39fe10864872c2b33c4de093cd086387d7e8b9
VoidExplorer <161141243+VoidExplorer@users.noreply.github.com>
2024-10-04 16:36:52 +0300

    feat: make operations function

--- javascript.js
+++ javascript.js
@@ -26,3 +26,39 @@ function operate(a, b, operator) {
             return divide(a, b);
     }
 }
+
+const screen = document.querySelector(".screen");
+
+screen.textContent = "0";
+let operator, a, b;
+const buttons = document.querySelector(".buttons-container");
+
+buttons.addEventListener("click", (e) => {
+  if (e.target.className === "operator") {
+    a = Number(screen.textContent);
+    operator = e.target.textContent;
+    return;
+  }
+
+  if (e.target.textContent === "=") {
+    if (!a) return;
+    b = Number(screen.textContent);
+    screen.textContent = operate(a, b, operator);
+    return;
+  }
+  if (screen.textContent === "0") {
+    if (e.target.textContent === "0") return;
+    screen.textContent = "";
+  }
+
+  if (a) screen.textContent = "";
+  if (screen.textContent.length > 5) return;
+  if (e.target.textContent == "AC") {
+    screen.textContent = "0";
+    a = undefined;
+    b = undefined;
+    return;
+  }
+  // console.log(e.target.textContent);
+  screen.textContent += e.target.textContent;
+});
